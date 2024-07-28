ad52333c538c43f81293f1eb967dac4e7be511d0
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-07-28 22:07:51 +0200

    Testing Flux

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,7 +18,7 @@ const App = () => {
 
     return <div>
         <div className="mb-3">
-            <label htmlFor="firstValue" className="form-label">First Value</label>
+            <label htmlFor="firstValue" className="form-label">First Value Test</label>
             <input
                 type="number"
                 className="form-control"
