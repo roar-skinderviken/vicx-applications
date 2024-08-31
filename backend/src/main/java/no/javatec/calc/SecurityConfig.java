2fc6c3269ad875dfac571f63f763e0d2d5f1f3e4
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-08-31 18:24:19 +0200

    Removed old stuff. Added CSP-headers

---
+++ backend/src/main/java/no/javatec/calc/SecurityConfig.java
@@ -0,0 +1,30 @@
+package no.javatec.calc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authz ->
+                authz.anyRequest().permitAll()
+        );
+
+        http.headers(headers ->
+                headers.contentSecurityPolicy(csp ->
+                        csp.policyDirectives(
+                                "default-src 'self'; " +
+                                        "script-src 'self'; " +
+                                        "style-src 'self'; " +
+                                        "img-src 'self';"
+                        ))
+        );
+        return http.build();
+    }
+}
