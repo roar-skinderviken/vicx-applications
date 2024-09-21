4ea371d4333843152af345e1e1df78a0221da2cc
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-09-21 13:08:32 +0200

    Refactored away duplicate code

--- pandascore/app.js
+++ pandascore/app.js
@@ -1,38 +1,29 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
-const port = 3000;
+const express = require("express")
+const axios = require("axios")
+const app = express()
+const port = 3000
 
-const apiKey = process.env.API_KEY;
+const apiKey = process.env.API_KEY
+const RUNNING_MATCH_TYPE = "running"
+const UPCOMING_MATCH_TYPE = "upcoming"
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-// Proxy request for running matches
-app.get('/api/csgo/matches/running', (req, res) => {
-    const url = `https://api.pandascore.co/csgo/matches/running?token=${apiKey}`;
+function handleProxyRequest(matchType, req, res) {
+    const url = `https://api.pandascore.co/csgo/matches/${matchType}?token=${apiKey}`
     axios.get(url)
-        .then(response => {
-            res.json(response.data);
-        })
-        .catch(error => {
-            res.status(error.response?.status || 500).send(error.message);
-        });
-});
+        .then(response => res.json(response.data))
+        .catch(error => res.status(error.response?.status || 500).send(error.message))
+}
 
-// Proxy request for upcoming matches
-app.get('/api/csgo/matches/upcoming', (req, res) => {
-    const url = `https://api.pandascore.co/csgo/matches/upcoming?token=${apiKey}`;
-    axios.get(url)
-        .then(response => {
-            res.json(response.data);
-        })
-        .catch(error => {
-            res.status(error.response?.status || 500).send(error.message);
-        });
-});
+// Health
+app.get("/", (req, res) =>
+    res.send("ALIVE"))
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+app.get("/api/csgo/matches/:type", (req, res) => {
+    const matchType = req.params.type
+    if (![RUNNING_MATCH_TYPE, UPCOMING_MATCH_TYPE].includes(matchType)) {
+        return res.status(404).send(`The match type ${matchType} is not supported`)
+    }
+    handleProxyRequest(matchType, req, res)
+})
+
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
