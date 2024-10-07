ff24bad033432311c472c650a4d735907a0166f0
victorskinderviken <vicdal66@gmail.com>
2024-10-07 22:02:02 +0200

    app.py back

--- backend-python/src/app.py
+++ backend-python/src/app.py
@@ -1,26 +1,40 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 import uvicorn
 from pydantic import BaseModel
 from src.kmeans_grades import map_scores_to_grades
 
 app = FastAPI()
 
-# Serve static files (HTML)
-app.mount("/html-pages", StaticFiles(directory="html-pages"), name="html-pages")
+# Comment in CORS when testing on localhost
+# origins = [
+#     "*"
+# ]
+#
+# # Add CORS middleware
+# # noinspection PyTypeChecker
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,  # List of allowed origins
+#     allow_credentials=True,
+#     allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
+#     allow_headers=["*"],  # Allows all headers
+# )
+
 
 @app.get("/health")
 async def health_check():
     return {"status": "UP"}
 
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+
 class KMeansRequest(BaseModel):
     failScore: int
     scores: list[int]
+
 
 @app.post("/k-means")
 async def post_kmeans(body: KMeansRequest) -> dict:
@@ -29,10 +43,6 @@ async def post_kmeans(body: KMeansRequest) -> dict:
         scores=body.scores
     )
 
-@app.get("/", response_class=HTMLResponse)
-async def get_html():
-    with open("html-pages/content/python.html") as f:
-        return f.read()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
