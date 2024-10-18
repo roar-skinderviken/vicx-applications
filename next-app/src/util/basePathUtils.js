a50129294990e9294bce73c21c9655275b42eec5
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-10-18 23:12:08 +0200

    NEXT_PUBLIC_CUSTOM_BASE_PATH

--- next-app/src/util/basePathUtils.js
+++ next-app/src/util/basePathUtils.js
@@ -1,5 +1,5 @@
 
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+const basePath = process.env.NEXT_PUBLIC_CUSTOM_BASE_PATH || '';
 
 export const urlFromBasePath = (url) => `${basePath}${url}`
