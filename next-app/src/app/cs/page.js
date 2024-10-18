885d005496c2de1397280f27d3b6d8ceace242cb
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-10-18 03:17:52 +0200

    Added custom bg image for cs page

--- next-app/src/app/cs/page.js
+++ next-app/src/app/cs/page.js
@@ -5,7 +5,7 @@ export const dynamic = "force-static"
 export default function CounterStrikePage() {
     return (
         <main className="content">
-            <div className="hero">
+            <div className="hero cs-hero">
                 <h1><span>Counter Strike Smokes</span></h1>
                 <p className="lead">Pick a map to get started!</p>
             </div>
