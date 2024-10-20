4ce44277ba27fba8500a0bf997f12b05d0f119e9
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-10-20 15:15:30 +0200

    Added redirects from old pages

--- next-app/next.config.mjs
+++ next-app/next.config.mjs
@@ -1,27 +1,76 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 
 const nextConfig = {
     async headers() {
         return [
             {
-                source: '/(.*)', // Apply to all routes
+                source: "/(.*)", // Apply to all routes
                 headers: [
                     {
-                        key: 'Cache-Control',
-                        value: 'public, max-age=3600',
+                        key: "Cache-Control",
+                        value: "public, max-age=3600",
                     },
                 ],
             },
             {
-                source: '/esport',
+                source: "/esport",
                 headers: [
                     {
-                        key: 'Cache-Control',
-                        value: 'public, max-age=30',
+                        key: "Cache-Control",
+                        value: "public, max-age=30",
                     },
                 ],
             },
         ]
+    },
+    async redirects() {
+        return [
+            {
+                source: "/arch.html",
+                destination: "/arch",
+                permanent: true
+            },
+            {
+                source: "/cs.html",
+                destination: "/cs",
+                permanent: true
+            },
+            {
+                source: "/esport.html",
+                destination: "/esport",
+                permanent: true
+            },
+            {
+                source: "/index.html",
+                destination: "/",
+                permanent: true
+            },
+            {
+                source: "/k-means.html",
+                destination: "/k-means",
+                permanent: true
+            },
+            {
+                source: "/microk8s.html",
+                destination: "/microk8s",
+                permanent: true
+            },
+            {
+                source: "/setup.html",
+                destination: "/microk8s",
+                permanent: true
+            },
+            {
+                source: "/pen.html",
+                destination: "/penetration-testing",
+                permanent: true
+            },
+            {
+                source: "/snake-game.html",
+                destination: "/snake",
+                permanent: true
+            },
+        ];
     },
 };
 
