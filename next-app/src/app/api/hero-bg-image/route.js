f4377040c1b6ed2c470dea55acec2d3f270de5e5
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-10-21 07:16:10 +0200

    Added revalidateTag when error

--- next-app/src/app/api/hero-bg-image/route.js
+++ next-app/src/app/api/hero-bg-image/route.js
@@ -1,10 +1,12 @@
 import * as path from "node:path"
 import * as fs from "node:fs"
+import {revalidateTag} from "next/cache"
 
 const IMAGE_EXPIRATION_IN_SECS = 900 // 15 minutes
 const IMAGE_URL = 'https://picsum.photos/1280/720'
 
 const FALLBACK_IMAGE_PATH = path.join(process.cwd(), 'public', 'images', 'hero-fallback.jpg')
+const FALLBACK_CACHE_TAG = "hero-image-cache"
 
 const buildResponse = (buffer) => new Response(
     buffer,
@@ -18,10 +20,16 @@ export async function GET() {
     try {
         const response = await fetch(
             IMAGE_URL,
-            {next: {revalidate: IMAGE_EXPIRATION_IN_SECS}}
+            {
+                next: {
+                    revalidate: IMAGE_EXPIRATION_IN_SECS,
+                    tags: [FALLBACK_CACHE_TAG]
+                }
+            }
         )
 
         if (!response.ok) {
+            await revalidateTag(FALLBACK_CACHE_TAG)
             return buildFallbackResponse()
         }
 
