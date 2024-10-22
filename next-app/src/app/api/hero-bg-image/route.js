f58edcd22658e4d65daecbc54a8578b04f0ed5ac
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-10-22 12:20:05 +0200

    Added cache: "force-cache" to be explicit. Not strictly necessary with next.revalidate

--- next-app/src/app/api/hero-bg-image/route.js
+++ next-app/src/app/api/hero-bg-image/route.js
@@ -21,6 +21,7 @@ export async function GET() {
         const response = await fetch(
             IMAGE_URL,
             {
+                cache: "force-cache",
                 next: {
                     revalidate: IMAGE_EXPIRATION_IN_SECS,
                     tags: [FALLBACK_CACHE_TAG]
