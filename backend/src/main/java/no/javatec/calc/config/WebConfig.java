fc4b00a8d79fa4418c3345a7a9173adeab12c29c
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-10-24 12:59:37 +0200

    Removed unused imports

--- backend/src/main/java/no/javatec/calc/config/WebConfig.java
+++ backend/src/main/java/no/javatec/calc/config/WebConfig.java
@@ -1,10 +1,9 @@
 package no.javatec.calc.config;
 
-import jakarta.validation.constraints.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
-import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -26,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "OPTIONS");
