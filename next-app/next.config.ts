68d8bd2a8972494a7bc82dcd2c392fb188c583ed
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-10-28 20:22:41 +0100

    Now using Dockerfile from docs

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: "standalone",  // Enable standalone output
+
     async headers() {
         return [
             {
