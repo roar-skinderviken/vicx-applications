8490eb0976da1b057bb8cafc788d5dc1e4cef465
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-11-09 00:34:08 +0100

    Small refactoring

--- next-app/src/app/api/calculator/route.ts
+++ next-app/src/app/api/calculator/route.ts
@@ -1,26 +1,22 @@
 import {NextRequest, NextResponse} from "next/server"
 import {getServerSession} from "next-auth"
 import authOptions from "@/authOptions"
+import {CustomSession} from "@/types/authTypes"
 
 const BACKEND_BASE_URL = process.env.TOMCAT_BACKEND_SECURED_URL || "http://localhost:8080/api-secured/calculator"
 
 export async function GET(request: NextRequest) {
-    const {searchParams} = new URL(request.url)
-
-    const firstValue = searchParams.get("first")
-    const secondValue = searchParams.get("second")
-    const operation = searchParams.get("operation")
-
-    const session = await getServerSession(authOptions)
-
-    // @ts-expect-error Because accessToken is not a prop of session
+    const session = (await getServerSession(authOptions)) as CustomSession | null
     const accessToken = session?.accessToken
 
     if (!accessToken) {
         return NextResponse.json({message: "Unauthorized"}, {status: 401})
     }
 
-    const url = `${BACKEND_BASE_URL}/${firstValue}/${secondValue}/${operation}`
+    const {first, second, operation} =
+        Object.fromEntries(new URL(request.url).searchParams.entries())
+
+    const url = `${BACKEND_BASE_URL}/${first}/${second}/${operation}`
 
     try {
         // Make a request to the Spring Boot OAuth2 token endpoint to refresh the token
