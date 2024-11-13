547f7c9cdbb60a72d07efc3e86310d6662df89c8
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-11-13 23:02:09 +0100

    Commented out Swagger UI

--- backend-spring-boot/build.gradle.kts
+++ backend-spring-boot/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+    //implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
     runtimeOnly("dev.akkinoc.spring.boot:logback-access-spring-boot-starter:4.3.2")
 
     implementation(project(":database"))
