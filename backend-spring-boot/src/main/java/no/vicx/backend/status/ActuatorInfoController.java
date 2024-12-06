b24e0e1bf6e53a334df5911bad03cb15a247079a
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-12-06 21:31:25 +0100

    Testing custom actuator info endpoint #2

--- backend-spring-boot/src/main/java/no/vicx/backend/status/ActuatorInfoController.java
+++ backend-spring-boot/src/main/java/no/vicx/backend/status/ActuatorInfoController.java
@@ -1,12 +1,14 @@
 package no.vicx.backend.status;
 
 import org.springframework.boot.actuate.info.InfoEndpoint;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Profile("!test")
 @RestController
 @RequestMapping("/info")
 public class ActuatorInfoController {
