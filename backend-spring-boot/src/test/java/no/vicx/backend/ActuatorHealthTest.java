0486d267894da1e0fc30be67713b2f012ed4647c
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-12-07 19:07:48 +0100

    Added db to readiness probe

--- backend-spring-boot/src/test/java/no/vicx/backend/ActuatorHealthTest.java
+++ backend-spring-boot/src/test/java/no/vicx/backend/ActuatorHealthTest.java
@@ -1,20 +1,28 @@
 package no.vicx.backend;
 
 import no.vicx.backend.testconfiguration.TestSecurityConfig;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalManagementPort;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpStatus;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 @Import(TestSecurityConfig.class)
 class ActuatorHealthTest {
 
@@ -22,22 +30,35 @@ class ActuatorHealthTest {
     private int managementPort;
 
     @Autowired
-    TestRestTemplate restTemplate;
-
-    private String baseManagementUrl;
-
-    @BeforeEach
-    void setup() {
-        baseManagementUrl = "http://localhost:" + managementPort + "/actuator/health";
-    }
+    WebTestClient webClient;
 
     @ParameterizedTest
-    @ValueSource(strings = {"/liveness", "/readiness"})
-    @DisplayName("Health Endpoint Probes Should Return HTTP 200")
-    void testHealthProbes(String probePath) {
-        var response = restTemplate.getForEntity(baseManagementUrl + probePath, String.class);
+    @MethodSource("probeEndpoints")
+    void getHealthProbe_givenExpectedComponents_expectComponentsAsInConfig(
+            String probeEndpoint, List<String> expectedComponents) {
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("{\"status\":\"UP\"}", response.getBody());
+        var currentProbeUrl = "http://localhost:%d/actuator/health%s"
+                .formatted(managementPort, probeEndpoint);
+
+        webClient.get()
+                .uri(currentProbeUrl)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(new ParameterizedTypeReference<Map<String, Object>>() {
+                })
+                .consumeWith(result -> {
+                    @SuppressWarnings({"unchecked", "DataFlowIssue"})
+                    var actualComponents = (Map<String, Object>) result.getResponseBody().get("components");
+
+                    assertEquals(expectedComponents.size(), actualComponents.size());
+                    assertThat(actualComponents.keySet(), containsInAnyOrder(expectedComponents.toArray()));
+                });
+    }
+
+    private static Stream<Arguments> probeEndpoints() {
+        return Stream.of(
+                Arguments.of("/readiness", List.of("db", "readinessState")),
+                Arguments.of("/liveness", Collections.singletonList("livenessState"))
+        );
     }
 }
