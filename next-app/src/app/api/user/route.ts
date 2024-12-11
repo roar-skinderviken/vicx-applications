303234adbc9e8b4296090c945c2be72aff3e4667
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-12-11 17:45:26 +0100

    Removed unused Next API endpoint. Updated middleware.ts

--- next-app/src/app/api/user/route.ts
+++ next-app/src/app/api/user/route.ts
@@ -6,6 +6,7 @@ const USER_ENDPOINT = "/api/user"
 export async function GET(request: NextRequest) {
     return forwardRequest(request, USER_ENDPOINT, "GET")
 }
+
 export async function PATCH(request: NextRequest) {
     return forwardRequest(request, USER_ENDPOINT, "PATCH", "application/json")
 }
--- next-app/src/middleware.ts
+++ next-app/src/middleware.ts
@@ -3,6 +3,6 @@ export {default} from "next-auth/middleware"
 export const config = {
     matcher: [
         "/user/dashboard",
-        "/user/image"
+        "/api/user:path*"
     ]
 }
