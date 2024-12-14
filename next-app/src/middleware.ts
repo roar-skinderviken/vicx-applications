7cb73e2acca112ed972da2854809952ef8f9b6d7
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-12-14 03:07:05 +0100

    Prepared middleware.ts for logging

--- next-app/src/middleware.ts
+++ next-app/src/middleware.ts
@@ -1,8 +1,19 @@
-export {default} from "next-auth/middleware"
+import nextAuthMiddleware from 'next-auth/middleware';
+import {NextRequest, NextResponse} from "next/server";
+
+const authConfigMatchers: string[] = [
+    "/user/dashboard",
+    "/api/user:path*"
+];
+
+export function middleware(req: NextRequest) {
+    if (authConfigMatchers.some(path => req.url.includes(path))) {
+        return nextAuthMiddleware(req as any);
+    }
+
+    return NextResponse.next();
+}
 
 export const config = {
-    matcher: [
-        "/user/dashboard",
-        "/api/user:path*"
-    ]
-}
+    matcher: ['/((?!_next|static|.*\\..*).*)']
+};
