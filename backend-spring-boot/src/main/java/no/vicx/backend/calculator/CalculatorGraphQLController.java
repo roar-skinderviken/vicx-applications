3761cf0281cb69c2ccc26cfb7694c026b2cb23f8
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-12-18 23:17:34 +0100

    Final fix for removing "anonymousUser"

--- backend-spring-boot/src/main/java/no/vicx/backend/calculator/CalculatorGraphQLController.java
+++ backend-spring-boot/src/main/java/no/vicx/backend/calculator/CalculatorGraphQLController.java
@@ -8,9 +8,11 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,7 +43,8 @@ public class CalculatorGraphQLController {
         return calculatorService.calculate(
                 new CalculatorRequestVm(firstValue, secondValue, operation),
                 Optional.ofNullable(authentication)
-                        .map(it -> it.isAuthenticated() ? it.getName() : null)
+                        .filter(auth -> !(auth instanceof AnonymousAuthenticationToken))
+                        .map(Principal::getName)
                         .orElse(null)
         );
     }
