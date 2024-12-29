e233a8bcd26020479d092599496e226ac2c7246b
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-12-29 11:28:02 +0100

    Testing matrix build

--- backend-spring-boot/src/main/java/no/vicx/backend/config/SecurityConfig.java
+++ backend-spring-boot/src/main/java/no/vicx/backend/config/SecurityConfig.java
@@ -35,8 +35,7 @@ public class SecurityConfig {
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "OPTIONS", "HEAD")
-                        .allowCredentials(true);
+                        .allowedMethods("GET", "POST", "OPTIONS", "HEAD");
             }
         };
     }
