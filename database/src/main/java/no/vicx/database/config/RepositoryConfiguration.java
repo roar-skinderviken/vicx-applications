0a45c00a9a87c8b2a27cd41bf885fe311776948e
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-02-23 03:12:52 +0100

    @AutoConfiguration for RepositoryConfiguration

--- database/src/main/java/no/vicx/database/config/RepositoryConfiguration.java
+++ database/src/main/java/no/vicx/database/config/RepositoryConfiguration.java
@@ -1,12 +1,12 @@
-package no.vicx.backend.config;
+package no.vicx.database.config;
 
 import no.vicx.database.calculator.CalcEntry;
 import no.vicx.database.user.VicxUser;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Configuration
+@AutoConfiguration
 @EnableJpaRepositories(basePackageClasses = {CalcEntry.class, VicxUser.class})
 @EntityScan(basePackageClasses = {CalcEntry.class, VicxUser.class})
 public class RepositoryConfiguration {
