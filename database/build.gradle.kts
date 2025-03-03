dd67df14be3b52904b83601992a23c2af1e97f3a
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-03-03 02:08:52 +0100

    Introduced RepositoryTestBase #2

--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -29,7 +29,6 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
-    testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
 }
 
--- database/src/test/java/no/vicx/database/RepositoryTestBase.java
+++ database/src/test/java/no/vicx/database/RepositoryTestBase.java
@@ -5,13 +5,11 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 
 @DataJpaTest
 public class RepositoryTestBase {
 
     @ServiceConnection
-    @Container
     private static final PostgreSQLContainer<?> POSTGRES =
             new PostgreSQLContainer<>("postgres:15-alpine");
 
