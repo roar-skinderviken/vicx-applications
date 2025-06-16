4df956ce0a932bb0d5c0473b77eed175bb7c12ce
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-06-16 23:20:09 +0200

    Testing WebClientConfig with custom codecs

--- backend-spring-boot/src/main/kotlin/no/vicx/backend/config/WebClientConfig.kt
+++ backend-spring-boot/src/main/kotlin/no/vicx/backend/config/WebClientConfig.kt
@@ -1,7 +1,10 @@
 package no.vicx.backend.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.codec.json.Jackson2JsonDecoder
+import org.springframework.http.codec.json.Jackson2JsonEncoder
 import org.springframework.web.reactive.function.client.WebClient
 
 
@@ -9,5 +12,14 @@ import org.springframework.web.reactive.function.client.WebClient
 class WebClientConfig {
 
     @Bean
-    fun webClient(builder: WebClient.Builder): WebClient = builder.build()
+    fun webClient(
+        builder: WebClient.Builder,
+        objectMapper: ObjectMapper
+    ): WebClient =
+        builder
+            .codecs { clientCodecConfigurer ->
+                clientCodecConfigurer.defaultCodecs().jackson2JsonDecoder(Jackson2JsonDecoder(objectMapper))
+                clientCodecConfigurer.defaultCodecs().jackson2JsonEncoder(Jackson2JsonEncoder(objectMapper))
+            }
+            .build()
 }
