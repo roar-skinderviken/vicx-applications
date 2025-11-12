078d690f17d40dc5cdbc221a10e15a8405c10862
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-11-12 22:43:28 +0100

    io.ktor:ktor-client-apache -> io.ktor:ktor-client-cio

--- backend-ktor/build.gradle.kts
+++ backend-ktor/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     implementation(libs.ktor.server.auth.jwt)
 
     implementation(libs.ktor.client.core)
-    implementation(libs.ktor.client.apache)
+    implementation(libs.ktor.client.cio)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.server.content.negotiation)
