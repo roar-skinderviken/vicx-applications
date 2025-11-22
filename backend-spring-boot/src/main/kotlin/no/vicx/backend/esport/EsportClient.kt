516b1c5c15e3b8916a143abfc818a8d0570cc291
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-11-22 14:31:33 +0100

    Revert "Disabling HTTP/3 for reactor.netty.http.client.HttpClient"
    This reverts commit d3cfec19e37e34d1372d1bda2eba6d86f34d7661.

--- backend-spring-boot/src/main/kotlin/no/vicx/backend/esport/EsportClient.kt
+++ backend-spring-boot/src/main/kotlin/no/vicx/backend/esport/EsportClient.kt
@@ -5,27 +5,18 @@ import no.vicx.backend.esport.vm.MatchType
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
-import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToFlux
 import reactor.core.publisher.Flux
-import reactor.netty.http.HttpProtocol
-import reactor.netty.http.client.HttpClient
 
 @Service
 class EsportClient(
     webClientBuilder: WebClient.Builder,
     @Value($$"${esport.token}") private val token: String,
 ) {
-    private val httpClient =
-        HttpClient
-            .create()
-            .protocol(HttpProtocol.H2, HttpProtocol.HTTP11)
-
     private val webClient =
         webClientBuilder
-            .clientConnector(ReactorClientHttpConnector(httpClient))
             .baseUrl(PANDASCORE_BASE_URL)
             .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
             .defaultHeader(HttpHeaders.AUTHORIZATION, "$BEARER_PREFIX$token")
